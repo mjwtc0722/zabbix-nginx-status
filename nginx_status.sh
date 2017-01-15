07b33a10fe455aa59837f92aa79e5c7f25132813
mjwtc0722 <310916789@qq.com>
2017-01-15 10:54:09 +0800

    Update nginx_status.sh

--- nginx_status.sh
+++ nginx_status.sh
@@ -6,25 +6,25 @@ function proc_num {
     num=$(pgrep nginx |wc -l)
 }
 function active {
-    num=$(curl "http://$HOST:$PORT/nginx_stat/" |grep 'Active' |awk '{print $NF}')
+    num=$(curl -s "http://$HOST:$PORT/nginx_stat" |grep 'Active' |awk '{print $NF}')
 }
 function reading {
-    num=$(curl "http://$HOST:$PORT/nginx_stat/" |grep 'Reading' |awk '{print $2}')
+    num=$(curl -s "http://$HOST:$PORT/nginx_stat" |grep 'Reading' |awk '{print $2}')
 }
 function writing {
-    num=$(curl "http://$HOST:$PORT/nginx_stat/" |grep 'Writing' |awk '{print $4}')
+    num=$(curl -s "http://$HOST:$PORT/nginx_stat" |grep 'Writing' |awk '{print $4}')
 }
 function waiting {
-    num=$(curl "http://$HOST:$PORT/nginx_stat/" |grep 'Waiting' |awk '{print $6}')
+    num=$(curl -s "http://$HOST:$PORT/nginx_stat" |grep 'Waiting' |awk '{print $6}')
 }
 function accepts {
-    num=$(curl "http://$HOST:$PORT/nginx_stat/" |awk NR==3 |awk '{print $1}')
+    num=$(curl -s "http://$HOST:$PORT/nginx_stat" |awk NR==3 |awk '{print $1}')
 }
 function handled {
-    num=$(curl "http://$HOST:$PORT/nginx_stat/" |awk NR==3 |awk '{print $2}')
+    num=$(curl -s "http://$HOST:$PORT/nginx_stat" |awk NR==3 |awk '{print $2}')
 }
 function requests {
-    num=$(curl "http://$HOST:$PORT/nginx_stat/" |awk NR==3 |awk '{print $3}')
+    num=$(curl -s "http://$HOST:$PORT/nginx_stat" |awk NR==3 |awk '{print $3}')
 }
 
 $1
